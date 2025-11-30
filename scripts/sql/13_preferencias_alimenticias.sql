c9624dbebda5deaffe5cdf1f956bcb548ad008e9
Maximo Perez <maximo@wyeworks.com>
2025-11-30 12:42:54 -0300

    Preferencias alimenticias

---
+++ scripts/sql/13_preferencias_alimenticias.sql
@@ -0,0 +1,32 @@
+USE das_ristorino;
+GO
+
+
+-- Insertar categoría de preferencias de Especialidades alimentarias
+INSERT INTO categorias_preferencias (nom_categoria) VALUES ('Especialidades alimentarias');
+
+DECLARE @cat_alimentacion VARCHAR(36);
+SELECT @cat_alimentacion = cod_categoria FROM categorias_preferencias WHERE nom_categoria = 'Especialidades alimentarias';
+
+
+-- Insertar traducciones de categorias alimenticias
+INSERT INTO idiomas_categorias_preferencias (cod_categoria, nro_idioma, categoria) VALUES (@cat_alimentacion, 0, 'Especialidades alimentarias');
+INSERT INTO idiomas_categorias_preferencias (cod_categoria, nro_idioma, categoria) VALUES (@cat_alimentacion, 1, 'Food specialties');
+
+-- Insertar dominios de categorias alimenticias
+INSERT INTO dominio_categorias_preferencias (cod_categoria, nro_valor_dominio, nom_valor_dominio) VALUES (@cat_alimentacion, 1, 'Vegetariano');
+INSERT INTO dominio_categorias_preferencias (cod_categoria, nro_valor_dominio, nom_valor_dominio) VALUES (@cat_alimentacion, 2, 'Vegano');
+INSERT INTO dominio_categorias_preferencias (cod_categoria, nro_valor_dominio, nom_valor_dominio) VALUES (@cat_alimentacion, 3, 'Sin gluten');
+INSERT INTO dominio_categorias_preferencias (cod_categoria, nro_valor_dominio, nom_valor_dominio) VALUES (@cat_alimentacion, 4, 'Sin lactosa');
+INSERT INTO dominio_categorias_preferencias (cod_categoria, nro_valor_dominio, nom_valor_dominio) VALUES (@cat_alimentacion, 5, 'Baja en calorías');
+INSERT INTO dominio_categorias_preferencias (cod_categoria, nro_valor_dominio, nom_valor_dominio) VALUES (@cat_alimentacion, 6, 'Organico');
+INSERT INTO dominio_categorias_preferencias (cod_categoria, nro_valor_dominio, nom_valor_dominio) VALUES (@cat_alimentacion, 7, 'Diabetico');
+
+-- Insertar traducciones de dominios de categorias alimenticias
+INSERT INTO idiomas_dominio_cat_preferencias (cod_categoria, nro_valor_dominio, nro_idioma, valor_dominio) VALUES (@cat_alimentacion, 1, 1, 'Vegetarian');
+INSERT INTO idiomas_dominio_cat_preferencias (cod_categoria, nro_valor_dominio, nro_idioma, valor_dominio) VALUES (@cat_alimentacion, 2, 1, 'Vegan');
+INSERT INTO idiomas_dominio_cat_preferencias (cod_categoria, nro_valor_dominio, nro_idioma, valor_dominio) VALUES (@cat_alimentacion, 3, 1, 'Gluten free');
+INSERT INTO idiomas_dominio_cat_preferencias (cod_categoria, nro_valor_dominio, nro_idioma, valor_dominio) VALUES (@cat_alimentacion, 4, 1, 'Lactose free');
+INSERT INTO idiomas_dominio_cat_preferencias (cod_categoria, nro_valor_dominio, nro_idioma, valor_dominio) VALUES (@cat_alimentacion, 5, 1, 'Low calorie');
+INSERT INTO idiomas_dominio_cat_preferencias (cod_categoria, nro_valor_dominio, nro_idioma, valor_dominio) VALUES (@cat_alimentacion, 6, 1, 'Organic');
+INSERT INTO idiomas_dominio_cat_preferencias (cod_categoria, nro_valor_dominio, nro_idioma, valor_dominio) VALUES (@cat_alimentacion, 7, 1, 'Diabetic');
